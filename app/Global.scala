6068f11865bfd41c163657561583e294c2c4cc80
Joao Vazão Vasques <joao.vasques@ist.utl.pt>
2013-12-22 23:36:31 +0000

    updates after rebase with dev

--- app/Global.scala
+++ app/Global.scala
@@ -2,13 +2,15 @@ import play.api.{GlobalSettings, Application}
 import com.google.inject._
 import play.api.Play
 import play.api.Play.current
+import service._
+import service.modules._
 import service.user.definitions._
 import service.user.implementations._
 import service.user.modules._
 
-
 object Global extends GlobalSettings {
   private lazy val injector = {
+    Guice.createInjector(new ApplicationModule)
     Guice.createInjector(new UserModule)
   }
 
--- app/service/implementations/ApplicationServiceImpl.scala
+++ app/service/implementations/ApplicationServiceImpl.scala
@@ -12,38 +12,38 @@ import se.radley.plugin.salat._
 import MongoContext._
 
 class ApplicationServiceImpl extends ApplicationService {
-	
-	private val dao = WazzaApplication.getDAO
+    
+    private val dao = WazzaApplication.getDAO
 
-	def insertApplication(application: WazzaApplication): Boolean = {
-		if(! exists(application.name)){
-			dao.insert(application)
-			true
-		} else {
-			false
-		}
-	}
+    def insertApplication(application: WazzaApplication): Boolean = {
+        if(! exists(application.name)){
+            dao.insert(application)
+            true
+        } else {
+            false
+        }
+    }
 
-	def deleteApplication(name: String): Unit = {
-		if(exists(name)){
-			val application = findBy("name", name)
-			dao.remove(application.head)
-		}
-	}
+    def deleteApplication(name: String): Unit = {
+        if(exists(name)){
+            val application = findBy("name", name)
+            dao.remove(application.head)
+        }
+    }
 
-	def exists(name: String): Boolean = {
-		! dao.findOne(MongoDBObject("name" -> name)).isEmpty
-	}
+    def exists(name: String): Boolean = {
+        ! dao.findOne(MongoDBObject("name" -> name)).isEmpty
+    }
 
-	def findBy(attribute: String, key: String): List[WazzaApplication] = {
-		dao.find(MongoDBObject(attribute -> key)).toList
-	}
+    def findBy(attribute: String, key: String): List[WazzaApplication] = {
+        dao.find(MongoDBObject(attribute -> key)).toList
+    }
 
-	def addItem(item: Item, applicationName: String): Boolean = {
+    def addItem(item: Item, applicationName: String): Boolean = {
         dao.update(
             MongoDBObject("name" -> applicationName),
             MongoDBObject("$push" -> MongoDBObject("items" -> grater[Item].asDBObject(item)))
         )
-		true
-	}
+        true
+    }
 }
