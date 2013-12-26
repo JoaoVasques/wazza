0d3f4243d77b36b4e297322074df9c54efd66e7c
Joao Vazão Vasques <joao.vasques@ist.utl.pt>
2013-12-26 15:14:57 +0000

    adds more implicit methods for json deseralization

--- modules/ApplicationModule/app/models/Item.scala
+++ modules/ApplicationModule/app/models/Item.scala
@@ -11,113 +11,113 @@ import se.radley.plugin.salat._
 import ApplicationMongoContext._
 
 case class Item(
-	id: String,
-	name: String,
-	description: String,
-	store: Int,
-	metadata: ItemMetadata,
-	currency: Currency
+  id: String,
+  name: String,
+  description: String,
+  store: Int,
+  metadata: ItemMetadata,
+  currency: Currency
 )
 
 case class Currency(
-	typeOf: Int, //virtual or real money
-	value: Double
+  typeOf: Int, //virtual or real money
+  value: Double
 )
 
 @Salat
 trait ItemMetadata {
-	def osType: String
-	def itemId: String
-	def title: String
-	def description: String
+  def osType: String
+  def itemId: String
+  def title: String
+  def description: String
 }
 
 case class GoogleMetadata(
-	override val osType: String,
-	override val itemId: String,
-	override val title: String,
-	override val description: String,
-	publishedState: String,
-	purchaseType: Int,
-	autoTranslate: Boolean,
-	locale: List[GoogleTranslations],
-	autofill: Boolean,
-	language: String,
-	price: Double
+  override val osType: String,
+  override val itemId: String,
+  override val title: String,
+  override val description: String,
+  publishedState: String,
+  purchaseType: Int,
+  autoTranslate: Boolean,
+  locale: List[GoogleTranslations],
+  autofill: Boolean,
+  language: String,
+  price: Double
 
 ) extends ItemMetadata
 
 case class GoogleTranslations(
-	locale: String,
-	title: String,
-	description: String
+  locale: String,
+  title: String,
+  description: String
 )
 
 case class AppleMetadata(
-	override val itemId: String,
-	override val title: String,
-	override val description: String,
-	productProperties: AppleProductProperties,
-	languageProperties: AppleLanguageProperties,
-	pricingProperties: ApplePricingProperties,
-	durationProperties: AppleDurationProperties
+  override val osType: String,
+  override val itemId: String,
+  override val title: String,
+  override val description: String,
+  productProperties: AppleProductProperties,
+  languageProperties: AppleLanguageProperties,
+  pricingProperties: ApplePricingProperties,
+  durationProperties: AppleDurationProperties
 
-)(implicit val osType: String = "Apple") extends ItemMetadata
+) extends ItemMetadata
 
 case class AppleProductProperties(
-	productType: Int, 
-	status: String,
-	reviewNotes: String
-	// screenshot: TODO
+  productType: Int, 
+  status: String,
+  reviewNotes: String
+  // screenshot: TODO
 )
 
 case class AppleLanguageProperties(
-	language: String,
-	display: String,
-	description: String,
-	publicationName: String
+  language: String,
+  display: String,
+  description: String,
+  publicationName: String
 )
 
 case class ApplePricingProperties(
-	clearedForSale: Boolean,
-	price: Double,
-	pricingAvailability: PricingAvailability
+  clearedForSale: Boolean,
+  price: Double,
+  pricingAvailability: PricingAvailability
 )
 
 case class PricingAvailability(
-	begin: Date,
-	end: Date
+  begin: Date,
+  end: Date
 )
 
 case class AppleDurationProperties(
-	autoRenewalDuration: Date,
-	freeTrialDuration: Date,
-	marketingIncentiveDuration: Date
+  autoRenewalDuration: Date,
+  freeTrialDuration: Date,
+  marketingIncentiveDuration: Date
 )
 
 object Item extends ModelCompanion[Item, ObjectId] {
 
-	val dao = new SalatDAO[Item, ObjectId](mongoCollection("applications")){}
+  val dao = new SalatDAO[Item, ObjectId](mongoCollection("applications")){}
 
-	def getDAO = dao
+  def getDAO = dao
 }
 
 package object ItemContext {
 
-	private val googleMetadataType = "models.application.GoogleMetadata"
-	private val appleMetadataType = "models.application.AppleMetadata"
+  private val googleMetadataType = "models.application.GoogleMetadata"
+  private val appleMetadataType = "models.application.AppleMetadata"
 
   implicit def jsonToItem(obj: Option[JsValue]): Option[Item] = {
       obj match {
           case Some(item) => {
-              println(item)
               Some(new Item(
-              	(item \ "_id").as[String],
-              	(item \ "name").as[String],
-              	(item \ "description").as[String],
-              	(item \ "store").as[Int],
-             		(item \ "metadata"),
-             		(item \ "currency")
+                (item \ "_id").as[String],
+                (item \ "name").as[String],
+                (item \ "description").as[String],
+                (item \ "store").as[Int],
+                (item \ "metadata"),
+                (item \ "currency")
               ))
           }
           case None => None 
@@ -125,46 +125,90 @@ package object ItemContext {
   }
 
   implicit def jsonToMetadata(obj: JsValue): ItemMetadata = {
-  	val metadataType = (obj \ "_t").as[String]
-  	if(metadataType == googleMetadataType){
-  		new GoogleMetadata(
-  			(obj \ "osType").as[String],
-  			(obj \ "itemId").as[String],
-  			(obj \ "title").as[String],
-  			(obj \ "description").as[String],
-  			(obj \ "publishedState").as[String],
-  			(obj \ "purchaseType").as[Int],
-  			(obj \ "autoTranslate").as[Boolean],
-  			(obj \ "locale"),
-  			(obj \ "autofill").as[Boolean],
-  			(obj \ "language").as[String],
-  			(obj \ "price").as[Double]
-  		)
-  	} else {
-  		//TODO: apple metadata
-			null
-  	}
+    val metadataType = (obj \ "_t").as[String]
+    if(metadataType == googleMetadataType){
+      new GoogleMetadata(
+        (obj \ "osType").as[String],
+        (obj \ "itemId").as[String],
+        (obj \ "title").as[String],
+        (obj \ "description").as[String],
+        (obj \ "publishedState").as[String],
+        (obj \ "purchaseType").as[Int],
+        (obj \ "autoTranslate").as[Boolean],
+        (obj \ "locale"),
+        (obj \ "autofill").as[Boolean],
+        (obj \ "language").as[String],
+        (obj \ "price").as[Double]
+      )
+    } else {
+      new AppleMetadata(
+        (obj \ "osType").as[String],
+        (obj \ "itemId").as[String],
+        (obj \ "title").as[String],
+        (obj \ "description").as[String],
+        (obj \ "productProperties"),
+        (obj \ "languageProperties"),
+        (obj \ "pricingProperties"),
+        (obj \ "languageProperties")
+      )
+    }
   }
 
   implicit def jsonToCurrency(obj: JsValue): Currency = {
-  	new Currency(
-  		(obj \ "typeOf").as[Int],
-  		(obj \ "value").as[Double]
-  	)
+    new Currency(
+      (obj \ "typeOf").as[Int],
+      (obj \ "value").as[Double]
+    )
   }
 
   implicit def jsonArrayToLocale(obj: JsValue): List[GoogleTranslations] = {
-  	obj match {
-  		case JsArray(array) => {
-  			array.map((element: JsValue) => {
-  				new GoogleTranslations(
-  						(element \ "locale").as[String],
-							(element \ "title").as[String],
-							(element \ "description").as[String]
-  				)
-  			}).toList
-  		}
-  		case _ => List[GoogleTranslations]()
-  	}
+    obj match {
+      case JsArray(array) => {
+        array.map((element: JsValue) => {
+          new GoogleTranslations(
+              (element \ "locale").as[String],
+              (element \ "title").as[String],
+              (element \ "description").as[String]
+          )
+        }).toList
+      }
+      case _ => List[GoogleTranslations]()
+    }
+  }
+
+  implicit def jsonToAppleProductProperties(obj: JsValue): AppleProductProperties = {
+    new AppleProductProperties(
+      (obj \ "productType").as[Int],
+      (obj \ "status").as[String],
+      (obj \ "reviewNotes").as[String]
+    )
+  }
+
+  implicit def jsonToAppleLanguageProperties(obj: JsValue): AppleLanguageProperties = {
+    new AppleLanguageProperties(
+      (obj \ "language").as[String],
+      (obj \ "display").as[String],
+      (obj \ "description").as[String],
+      (obj \ "publicationName").as[String]
+    )
+  }
+
+  implicit def jsonToApplePricingProperties(obj: JsValue): ApplePricingProperties = {
+    new ApplePricingProperties(
+      (obj \ "clearedForSale").as[Boolean],
+      (obj \ "price").as[Double],
+      new PricingAvailability(
+        (obj \ "pricingAvailability" \ "begin").as[Date],
+        (obj \ "pricingAvailability" \ "end").as[Date]
+      )
+    )
+  }
+
+  implicit def jsonToAppleDurationProperties(obj: JsValue): AppleDurationProperties = {
+    new AppleDurationProperties(
+      (obj \ "autoRenewalDuration").as[Date],
+      (obj \ "freeTrialDuration").as[Date],
+      (obj \ "marketingIncentiveDuration").as[Date]
+    )
   }
 }
