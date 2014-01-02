2db0f5e4d68877621bd97632b13eedc01427a27c
Joao Vazão Vasques <joao.vasques@ist.utl.pt>
2014-01-02 16:16:32 +0000

    adds validation service

--- modules/ApplicationModule/app/assets/javascripts/application/service.js
+++ modules/ApplicationModule/app/assets/javascripts/application/service.js
@@ -1,1 +1,22 @@
 // application module
+
+angular.module('ApplicationModule.services', [])
+  .factory('readyToSubmitService', function() {
+    return {
+      validate: function(formData){
+        if (formData.name != "" &&
+            formData.url != "" &&
+            formData.appType != ""
+        ) {
+          if (formData.appType == "Android" && formData.androidData.packageName != "") {
+            return false;
+          } else {
+            // TODO: iOS stuff
+            return true;
+          }
+        } else {
+          return true;
+        }
+      }
+    };
+});
