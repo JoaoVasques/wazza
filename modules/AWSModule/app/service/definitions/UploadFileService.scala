234df7d45a28bd442405fdbdf70430e998805082
Joao Vazão Vasques <joao.vasques@ist.utl.pt>
2014-01-16 17:51:44 +0000

    Adds implementation of upload to S3.

--- modules/AWSModule/app/service/definitions/UploadFileService.scala
+++ modules/AWSModule/app/service/definitions/UploadFileService.scala
@@ -2,9 +2,17 @@ package service.aws.definitions
 
 import scala.util.{Try, Success, Failure}
 import scala.concurrent._
+import com.amazonaws.services.s3.model._
+import play.api.Play
+import java.io.File
 
 trait UploadFileService {
 
-  def upload(): Future[Try[String]]
+  protected val PhotosBucket: Try[String] = Play.current.configuration.getConfig("aws") match {
+      case Some(config) => Success(config.underlying.root.get("photosBucket").render)
+      case _ => Failure(new Exception("AWS Credentials do not exist"))
+  }
+
+  def upload(file: File): Future[PutObjectResult]
 
 }
--- modules/AWSModule/app/service/implementations/UploadFileServiceImpl.scala
+++ modules/AWSModule/app/service/implementations/UploadFileServiceImpl.scala
@@ -5,8 +5,15 @@ import scala.util.{Try, Success, Failure}
 import play.api.Play
 import com.amazonaws.auth._
 import com.amazonaws.services.s3._
+import com.amazonaws.services.s3.model._
+import com.amazonaws.AmazonServiceException
 import scala.concurrent._
 import ExecutionContext.Implicits.global
+import play.api.mvc.MultipartFormData._
+import java.io.File
+import models.aws._
+import play.api.http.Status
+import java.io.FileInputStream
 
 class UploadFileServiceImpl extends UploadFileService {
 
@@ -23,24 +30,34 @@ class UploadFileServiceImpl extends UploadFileService {
     }
   }
 
-  private def getS3Client(bucketName: String, key: String): Try[AmazonS3Client] = {
+  private def getS3Client(bucketName: String): Try[AmazonS3Client] = {
     getAWSCredentials match {
       case Success(credentialData) => {
-        val myCredentials = new BasicAWSCredentials(credentialData("myAccessKeyID"), credentialData("mySecretKey"))
+        val myCredentials = new BasicAWSCredentials(credentialData("accessKeyId"), credentialData("secretKey"))
         Success(new AmazonS3Client(myCredentials))
       }
       case Failure(failure) => Failure(failure)
     }
   }
 
-  def upload(): Future[Try[String]] = {
-    getAWSCredentials match {
-      case Success(credentials) => {
-        println(credentials)
-        null
+  def upload(file: File): Future[PutObjectResult] = {
+    val promise = Promise[PutObjectResult]
+
+    Future {
+      PhotosBucket match {
+        case Success(bucket) => {
+          try {
+              val s3Client = getS3Client(bucket).get
+              s3Client.putObject(new PutObjectRequest(bucket, file.getName, new FileInputStream(file), new ObjectMetadata()))
+            } catch {
+              case err: AmazonServiceException if err.getStatusCode == Status.NOT_FOUND => promise.failure(new S3NotFound(bucket, file.getName))
+              case err: Throwable => promise.failure(new S3Failed(err))
+            }
+        }
+        case Failure(failure) => promise.failure(failure)
       }
-      case Failure(_) => null
     }
+
+    promise.future
   }
-  
 }
