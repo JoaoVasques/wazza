e45a38bbb41886e7aaba2415d85c7705af20ed9d
Joao Vazão Vasques <joao.vasques@ist.utl.pt>
2014-01-19 00:42:47 +0000

    Removes hashtags from urls

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -3,16 +3,22 @@
 
 // Declare app level module which depends on filters, and services
 angular.module('Wazza', ['ngRoute', 'Wazza.controllers', 'ItemModule']).
-  config([ '$routeProvider', function($routeProvider) {
-      $routeProvider.when('/home', {
+  config([ '$routeProvider', '$locationProvider' , function($routeProvider, $locationProvider) {
+    $routeProvider
+      .when('/xpto', {
+        templateUrl: '/splash',
+        controller: ''
+      })
+      .when('/home', {
         templateUrl : '/app/new',
         controller : ''
       })
       .when('/item/create/:applicationId', {
         templateUrl: '/app/item/new/Android',
         controller: 'NewItemController'
-      })
-      ;
+      });
+
+    $locationProvider.html5Mode(true);
     }
   ])
 ;
