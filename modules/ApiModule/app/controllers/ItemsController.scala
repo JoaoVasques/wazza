2232b7e7f488cd0679936924cbb9684845921ed8
Joao Vazão Vasques <joao.vasques@ist.utl.pt>
2014-02-10 18:26:36 +0000

    updates purchase data and finishes implementation of create purchase data endpoint

--- modules/ApiModule/app/controllers/ItemsController.scala
+++ modules/ApiModule/app/controllers/ItemsController.scala
@@ -2,15 +2,23 @@ package controllers.api
 
 import com.google.inject._
 import models.application.Item
+import models.application.{PurchaseInfo}
+import models.application.{LocationInfo}
 import play.api._
+import play.api.libs.json.JsError
+import play.api.libs.json.JsSuccess
 import play.api.libs.json.{JsArray, Json}
 import play.api.mvc._
 import scala.concurrent.ExecutionContext.Implicits.global
+import scala.util.Failure
+import scala.util.Success
 import service.application.definitions.ApplicationService
 import service.security.definitions.TokenManagerService
+import service.application.definitions.{PurchaseService}
 
 class ItemsController @Inject()(
-  applicationService: ApplicationService
+  applicationService: ApplicationService,
+  purchaseService: PurchaseService
 ) extends Controller {
 
   def getItems(applicationName: String) = Action {implicit request =>
@@ -35,14 +43,30 @@ class ItemsController @Inject()(
   }
   
   def handlePurchase = Action(parse.json) {implicit request =>
-//    request.body.
-    /**
-      Information:
-        - price: Double
-        - time: String
-        - location: {latitude, longitude}
-    **/
-    Ok
+
+    if(applicationService.itemExists(
+      (request.body \ "itemId").as[String],
+      (request.body \ "applicationName").as[String]
+    )) {
+
+      val purchaseInfo = new PurchaseInfo(
+        (request.body \ "id").as[String],
+        (request.body \ "applicationName").as[String],
+        (request.body \ "itemId").as[String],
+        (request.body \ "price").as[Double],
+        (request.body \ "time").as[String],
+        (request.body \ "location").validate[LocationInfo] match {
+          case success: JsSuccess[LocationInfo] => Some(success.value)
+          case JsError(errors) => None
+        }
+      )
+      purchaseService.save(purchaseInfo) match {
+        case Success(_) => Ok
+        case Failure(_) => BadRequest
+      }
+    } else {
+      BadRequest
+    }
   }
 }
 
