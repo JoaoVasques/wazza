9ba10807370af14beccf9c910dae72e4b267400d
Joao Vazão Vasques <joao.vasques@ist.utl.pt>
2014-04-09 22:24:18 +0100

    updates test class

--- modules/ApplicationModule/test/PurchaseServiceTest.scala
+++ modules/ApplicationModule/test/PurchaseServiceTest.scala
@@ -1,31 +1,38 @@
 package test.application
 
-import models.application.{LocationInfo, PurchaseInfo}
+import models.user.DeviceInfo
+import models.user.DeviceInfo
+import models.user.LocationInfo
 import org.specs2.mutable._
 import play.api.test._
 import play.api.test.Helpers._
 import scala.util.Failure
 import scala.util.Success
 import com.google.inject._
-import service.application.implementations.{PurchaseServiceImpl}
 import service.persistence.implementations.{MongoDatabaseService}
+import service.user.implementations.MobileUserServiceImpl
+import service.user.implementations.PurchaseServiceImpl
+import models.user.PurchaseInfo
 
 class PurchaseServiceTest extends Specification {
 
   private object PurchaseData {
     val id = "id"
+    val userId = "user"
     val applicationName = "app name"
     val itemId = "itemId"
     val price = 1.99
     val time = "today"
     val location = new LocationInfo(1,2)
+    val deviceInfo = new DeviceInfo("iOS", "iPhone", "5s", "model")
   }
 
   private def initPurchaseService(): PurchaseServiceImpl = {
     val uri = "mongodb://localhost:27017/wazza-test"
     val mongoDbService = new MongoDatabaseService
     mongoDbService.init(uri, PurchaseInfo.PurchaseCollection)
-    new PurchaseServiceImpl(mongoDbService)
+    val userService = new MobileUserServiceImpl(mongoDbService)
+    new PurchaseServiceImpl(userService, mongoDbService)
   }
 
   "Purchase Operations" should {
@@ -33,22 +40,24 @@ class PurchaseServiceTest extends Specification {
       val purchaseService = initPurchaseService
       val purchase = new PurchaseInfo(
         PurchaseData.id,
+        PurchaseData.userId,
         PurchaseData.applicationName,
         PurchaseData.itemId,
         PurchaseData.price,
         PurchaseData.time,
+        PurchaseData.deviceInfo,
         Some(PurchaseData.location)
       )
-
+    
       "Insert" in {
-        purchaseService.save(purchase) must equalTo(Success())
-        purchaseService.exist(PurchaseData.id) must equalTo(true)
-        purchaseService.get(PurchaseData.id).get must equalTo(purchase)
+        purchaseService.save(purchase, PurchaseData.userId) must equalTo(Success())
+        purchaseService.exist(PurchaseData.id, PurchaseData.userId) must equalTo(true)
+        purchaseService.get(PurchaseData.id, PurchaseData.userId).get must equalTo(purchase)
       }
 
       "Delete" in {
-        purchaseService.delete(purchase) must equalTo(Success())
-        purchaseService.get(PurchaseData.id) must equalTo(None)
+        purchaseService.delete(purchase, PurchaseData.userId) must equalTo(Success())
+        purchaseService.get(PurchaseData.id, PurchaseData.userId) must equalTo(None)
       }
     }
   }
