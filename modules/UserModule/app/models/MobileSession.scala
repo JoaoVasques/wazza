a956ec4f03b29b6c2bbbfe72fd042dd377e518aa
Joao Vazão Vasques <joao.vasques@ist.utl.pt>
2014-04-13 16:06:33 +0100

    removes unnecessary attributes from mobile session

--- modules/UserModule/app/models/MobileSession.scala
+++ modules/UserModule/app/models/MobileSession.scala
@@ -6,7 +6,6 @@ import play.api.Play.current
 import play.api.libs.json._
 
 case class MobileSession(
-  userId: String,
   length: Double,
   startTime: String,
   deviceInfo: DeviceInfo
@@ -15,14 +14,12 @@ case class MobileSession(
 object MobileSession {
 
   implicit val readJson = (
-    (__ \ "userId").read[String] and
     (__ \ "sessionLength").read[Double] and
     (__ \ "startTime").read[String] and
     (__ \ "deviceInfo").read[DeviceInfo]
   )(MobileSession.apply _)
 
   implicit val buildFromJson = (
-    (__ \ "userId").write[String] and
     (__ \ "sessionLength").write[Double] and
     (__ \ "startTime").write[String] and
     (__ \ "deviceInfo").write[DeviceInfo]
