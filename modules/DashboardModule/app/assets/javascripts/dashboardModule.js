3e26e9f675ad1b2513b87360c930c1d8f769b11a
Duarte Barbosa <duarte.barbosa@gmail.com>
2014-05-20 17:22:59 +0100

    added users chart

--- modules/DashboardModule/app/assets/javascripts/dashboardModule.js
+++ modules/DashboardModule/app/assets/javascripts/dashboardModule.js
@@ -95,7 +95,7 @@ angular.module('DashboardModule', ['ItemModule.services'])
     
             series: [{
                 type: 'area',
-                name: 'USD to EUR',
+                name: 'USD',
                 pointInterval: 24 * 3600 * 1000,
                 pointStart: 1328004400000,
                 data: [
@@ -264,7 +264,7 @@ angular.module('DashboardModule', ['ItemModule.services'])
     
             series: [{
                 type: 'area',
-                name: 'USD to EUR',
+                name: 'USD',
                 pointInterval: 24 * 3600 * 1000,
                 pointStart: 1328004400000,
                 data: [
@@ -593,6 +593,50 @@ angular.module('DashboardModule', ['ItemModule.services'])
       }]
     });
   });
+
+$(function () {
+        $('#users').highcharts({
+            title: {
+                text: 'Users',
+                x: -20 //center
+            },
+            xAxis: {
+                categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun',
+                    'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+            },
+            yAxis: {
+                title: {
+                    text: '# number'
+                },
+                plotLines: [{
+                    value: 0,
+                    width: 1,
+                    color: '#808080'
+                }]
+            },
+            tooltip: {
+                valueSuffix: '°C'
+            },
+            legend: {
+                layout: 'vertical',
+                align: 'right',
+                verticalAlign: 'middle',
+                borderWidth: 0
+            },
+            series: [{
+                name: 'Total Downloads',
+                data: [7.0, 6.9, 9.5, 14.5, 18.2, 21.5, 25.2, 26.5, 23.3, 18.3, 13.9, 9.6]
+            }, {
+                name: 'Active Users',
+                data: [-0.2, 0.8, 5.7, 11.3, 17.0, 22.0, 24.8, 24.1, 20.1, 14.1, 8.6, 2.5]
+            }, {
+                name: 'Active Users with IAP',
+                data: [3.9, 4.2, 5.7, 8.5, 11.9, 15.2, 17.0, 16.6, 14.2, 10.3, 6.6, 4.8]
+            }]
+        });
+    });
+    
+
 
 $(function () {
     
