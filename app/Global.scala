35c690d9fe586ff45f2127fda17946831bcc726f
Joao Vasques <joao@usewazza.com>
2014-06-22 11:21:02 +0100

    fixes syntax error

--- app/Global.scala
+++ app/Global.scala
@@ -43,7 +43,8 @@ object Global extends GlobalSettings {
     Future.successful(NotFound(
       views.html.index()
     ))
-
+  }
+  
   override def onStart(app:Application) = {
     //val analyticsJobScheduler = Akka.system.actorOf(Props[AnalyticsJobSchedulerActor], name = "analytics-job-scheduler")
     //Akka.system.scheduler.schedule(0.microsecond, 60.second, analyticsJobScheduler, TopItems)
