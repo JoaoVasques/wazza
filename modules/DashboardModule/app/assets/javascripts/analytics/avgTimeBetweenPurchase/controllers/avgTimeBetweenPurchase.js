192904ab6bd5452cb08a1f6d2c5146f52af6b553
Duarte Barbosa <duarte.barbosa@gmail.com>
2014-09-25 15:52:16 +0100

    refactor

--- modules/DashboardModule/app/assets/javascripts/analytics/avgTimeBetweenPurchase/controllers/avgTimeBetweenPurchase.js
+++ modules/DashboardModule/app/assets/javascripts/analytics/avgTimeBetweenPurchase/controllers/avgTimeBetweenPurchase.js
@@ -21,18 +21,19 @@ dashboard
     ATBPDateChanged
   ) {
 
-    TopbarService.setName("Average Time Between Purchases");
-    $scope.context = new DetailedKpiModel(DateModel.startDate, DateModel.endDate, "Average Time Between Purchases");
-
+    var title = "Average Time Between Purchases";
     var KpiId = "avgTimeBetweenPurchase";
 
-    $scope.updateChart("Average Revenue Per User", $scope.context);
-    $scope.updateOnChangedDate($scope.context, KpiId, "Average Time Between Purchases");
+    TopbarService.setName(title);
+    $scope.context = new DetailedKpiModel(DateModel.startDate, DateModel.endDate, title);
+
+    $scope.updateChart(title, $scope.context);
+    $scope.updateOnChangedDate($scope.context, KpiId, title);
 
     $scope.$on(ArpuDateChanged, function(ev, data) {
       $scope.context.beginDate = DateModel.startDate;
       $scope.context.endDate = DateModel.endDate;
-      $scope.updateOnChangedDate($scope.context, KpiId, "Average Time Between Purchases");
+      $scope.updateOnChangedDate($scope.context, KpiId, title);
     });
 
 }]);
