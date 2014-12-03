008f820f2b184abd0471150dd05d44d2ca99f8b9
Duarte Barbosa <duarte.barbosa@gmail.com>
2014-12-03 20:43:08 +0000

    again

--- public/javascript/app/controllers/appController.js
+++ public/javascript/app/controllers/appController.js
@@ -47,7 +47,6 @@ application.controller('AppController', [
       ApplicationStateService.cleanup();
       $scope.authOK = false;
 
-      document.body.className = "skin-blue";
       $state.go("webframe.login");
       //$state.go(url.value);      //TODO: fix this. url.value returns the relative url instead of the state
     });
