61fa026d8d09d6bb27b2f584a2d21522884f8465
Joao Vasques <joao.vasques@tecnico.ulisboa.pt>
2015-03-31 23:26:13 +0100

    adds payment information on the frontend and backup logic to save 3rd party secrets

---
+++ modules/payments/app/PaymentTypes.scala
@@ -0,0 +1,9 @@
+package payments
+
+object PaymentTypes {
+
+  val InAppPurchases = 1
+  val PayPal = 2
+  val Stripe = 3 //TODO
+}
+
--- public/javascript/dashboard/settings/controllers/settingsController.js
+++ public/javascript/dashboard/settings/controllers/settingsController.js
@@ -13,6 +13,7 @@ dashboard.controller('SettingsController', [
     "$q",
     "CurrencyService",
     "CurrencyChanges",
+    "UpdatePaymentCredentialsService",
     function (
         $scope,
         $location,
@@ -27,12 +28,16 @@ dashboard.controller('SettingsController', [
         KpiModel,
         $q,
         CurrencyService,
-      CurrencyChanges
+      CurrencyChanges,
+      UpdatePaymentCredentialsService
         ) {
 
         var bootstrapSuccessCallback = function (data) {
-
             $scope.credentials = data.data.credentials;
+            /** PayPal Credentials **/
+            if(data.data.hasOwnProperty('payPalCredentials')) {
+              $scope.payPalCredentials = data.data.payPalCredentials;
+            }
 
             $scope.userInfo = data.data.userInfo
             $scope.userInfo.companyName = ApplicationStateService.getCompanyName();
@@ -56,12 +61,21 @@ dashboard.controller('SettingsController', [
             bootstrapSuccessCallback,
             bootstrapFailureCallback);
 
-        $scope.currencies = CurrencyService.getCurrencies();
+      $scope.currencies = CurrencyService.getCurrencies();
       $scope.currentCurrency = ApplicationStateService.currency.name;
       $scope.changeCurrency = function(newCurrency) {
         $scope.currentCurrency = newCurrency;
         ApplicationStateService.changeCurrency(CurrencyService.getCurrency($scope.currentCurrency));
-      }
-      
+      };
+
+      $scope.savePaymentChanges = function() {
+        $scope.payPalCredentials.paymentSystem = 2;
+        UpdatePaymentCredentialsService.execute(
+          $scope.payPalCredentials,
+          function(data) {console.log("SUCCESS");},
+          function(err) {console.log("ERROR");}
+        )
+
+      };
 }]);
 
