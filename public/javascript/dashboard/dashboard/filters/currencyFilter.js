d2f12149ebe1b743482af113197557f24c5f444d
Joao Vasques <joao.vasques@tecnico.ulisboa.pt>
2015-05-20 19:43:18 +0200

    saves work

--- public/javascript/dashboard/dashboard/filters/currencyFilter.js
+++ public/javascript/dashboard/dashboard/filters/currencyFilter.js
@@ -11,13 +11,12 @@ dashboard.filter('wzCurrencyFilter', [
     var out = "";
     var kpis = [
       'Total Revenue', 'Avg Revenue Per User', 'Avg Revenue per Session',
-      'Average Revenue Per User', 'Average Revenue Per Session'
+      'Average Revenue Per User', 'Average Revenue Per Session', 'Life Time Value'
     ];
     if(_.contains(kpis, kpiName)) {
       switch(ApplicationStateService.currency.name) {
         case 'Euro':
             out = CurrencyService.getCurrency('Euro').symbol + " " + input;
-          //out = input + " " + CurrencyService.getCurrency('Euro').symbol;
           break;
         case 'Dollar':
             out = CurrencyService.getCurrency('Dollar').symbol + " " + input;
